5daa2a56ea385bd0ec56e5aab3bac60414b85bfb
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-08-01 17:56:11 +0700

    fintech magazine

---
+++ pages/projects/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Header from "../../components/Header";
+import Magazine from "../../components/Magazine";
+
+export default function Projects() {
+    return (
+        <>
+            <Head>
+                <title>Our projects</title>
+                <meta name="description" content="RMIT FinTech Club projects" />
+                <link rel="icon" href="/icon.png" />
+            </Head>
+            <Header />
+            <div className="container pt-5">
+                <Magazine />
+                <Magazine />
+            </div>
+        </>
+    );
+}

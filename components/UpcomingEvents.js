59e4831eab34dd38cb9a0f1a55b3129e64460285
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-08-01 15:54:34 +0700

    Add prettier and format all files

--- components/UpcomingEvents.js
+++ components/UpcomingEvents.js
@@ -1,53 +1,59 @@
-import React from 'react'
-import { Row, Col, Container, Button } from 'react-bootstrap'
-import Image from 'next/image'
+import React from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import Image from "next/image";
 
 const UpcomingEvents = () => {
     return (
-        <section className='' data-aos='fade-up' id="upcoming">
+        <section className="" data-aos="fade-up" id="upcoming">
             <div className="container-fluid">
-            <Row>
-                <Col lg={4} className="upcoming-col">
-                    <div className="bg-secondary ft-shadow-1 py-4 h-100 inverted-border-radius" data-aos="slide-up" data-aos-duration="6000">
-                        <h1 className='display-1 my-0 text-white text-center'>20+</h1>
-                        <h1 className='fs-2 my-0 text-white text-center text-uppercase'>projects</h1>
-                        {/* <p className='my-0 fs-1 text-uppercase text-white'>projects</p> */}
-                    </div>
-                    <div className="bg-ft-skyblue ft-shadow-1 py-4 h-100 inverted-border-radius-2" data-aos="slide-up" data-aos-duration="6000">
-                        <h1 className='display-1 my-0 text-white text-center'>100+</h1>
-                        <h1 className='fs-2 my-0 text-white text-center text-uppercase'>members</h1>
-                        {/* <p className='my-0 fs-1 text-uppercase text-white'>members</p> */}
-                    </div>
-                    <div className="bg-ft-gray ft-shadow-1 py-4 h-100 inverted-border-radius-3" data-aos="slide-up" data-aos-duration="6000">
-                        <h1 className='display-1 my-0 text-white text-center'>4</h1>
-                        <h1 className='fs-2 my-0 text-white text-center text-uppercase'>departments</h1>
-                        {/* <p className='my-0 fs-1 text-uppercase text-white'>departments</p> */}
+                <Row>
+                    <Col lg={4} className="upcoming-col">
+                        <div
+                            className="bg-secondary ft-shadow-1 py-4 h-100 inverted-border-radius"
+                            data-aos="slide-up"
+                            data-aos-duration="6000"
+                        >
+                            <h1 className="display-1 my-0 text-white text-center">20+</h1>
+                            <h1 className="fs-2 my-0 text-white text-center text-uppercase">projects</h1>
+                            {/* <p className='my-0 fs-1 text-uppercase text-white'>projects</p> */}
+                        </div>
+                        <div
+                            className="bg-ft-skyblue ft-shadow-1 py-4 h-100 inverted-border-radius-2"
+                            data-aos="slide-up"
+                            data-aos-duration="6000"
+                        >
+                            <h1 className="display-1 my-0 text-white text-center">100+</h1>
+                            <h1 className="fs-2 my-0 text-white text-center text-uppercase">members</h1>
+                            {/* <p className='my-0 fs-1 text-uppercase text-white'>members</p> */}
+                        </div>
+                        <div
+                            className="bg-ft-gray ft-shadow-1 py-4 h-100 inverted-border-radius-3"
+                            data-aos="slide-up"
+                            data-aos-duration="6000"
+                        >
+                            <h1 className="display-1 my-0 text-white text-center">4</h1>
+                            <h1 className="fs-2 my-0 text-white text-center text-uppercase">departments</h1>
+                            {/* <p className='my-0 fs-1 text-uppercase text-white'>departments</p> */}
+                        </div>
+                    </Col>
 
-                    </div>
-                </Col>
+                    <Col lg={8} className="upcoming-col upcoming-col-2-bg">
+                        <div className="d-flex justify-content-end">
+                            <h1 className="display-1 text-right">Upcoming Events</h1>
+                        </div>
 
-                <Col lg={8} className="upcoming-col upcoming-col-2-bg">
-                    <div className="d-flex justify-content-end">
-                        <h1 className="display-1 text-right">
-                            Upcoming Events
-                        </h1>
-                    </div>
+                        <div className="d-flex justify-content-end">
+                            <p className="text-secondary fs-3">DON'T MISS OUT!</p>
+                        </div>
 
-                    <div className="d-flex justify-content-end">
-                        <p className="text-secondary fs-3">
-                            DON'T MISS OUT!
-                        </p>
-                    </div>
-
-                    <div className="d-flex justify-content-end">
-                        <div className="border w-50 border-ft-gray"></div>
-                    </div>
-
-                </Col>
-            </Row>
+                        <div className="d-flex justify-content-end">
+                            <div className="border w-50 border-ft-gray"></div>
+                        </div>
+                    </Col>
+                </Row>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default UpcomingEvents
+export default UpcomingEvents;
--- components/Welcome.js
+++ components/Welcome.js
@@ -1,52 +1,92 @@
-import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
-import Image from 'next/image'
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Image from "next/image";
 
 const Welcome = () => {
     return (
-        <section data-aos='fade-up' id="home" className="">
-            <div className='container-fluid d-none d-lg-block'>
+        <section data-aos="fade-up" id="home" className="">
+            <div className="container-fluid d-none d-lg-block">
                 <Row>
                     <Col lg={6} md={1} className="pt-7 welcome-col-1-bg welcome-col-1">
-                        <div className='d-flex pe-2'>
-                            <h1 className='display-3'><span className="text-secondary">Fin</span>ance is our root<br /><span className="text-secondary">Tech</span>nology is our weapon</h1>
+                        <div className="d-flex pe-2">
+                            <h1 className="display-3">
+                                <span className="text-secondary">Fin</span>ance is our root
+                                <br />
+                                <span className="text-secondary">Tech</span>nology is our weapon
+                            </h1>
                         </div>
-                        <div className='d-flex'>
-                            <p className='text-secondary fs-3'>RMIT VIETNAM FINTECH CLUB</p>
+                        <div className="d-flex">
+                            <p className="text-secondary fs-3">RMIT VIETNAM FINTECH CLUB</p>
                         </div>
-                        <div className='d-flex justify-content-center pe-5'>
-                            <p className=''>RMIT Vietnam FinTech Club was launched with the goal to inspire, educate and increase the exposure of people to FinTech and digital disruption via our workshops, meetups, page contents, conferences, bootcamps, and events.</p>
+                        <div className="d-flex justify-content-center pe-5">
+                            <p className="">
+                                RMIT Vietnam FinTech Club was launched with the goal to inspire, educate and increase
+                                the exposure of people to FinTech and digital disruption via our workshops, meetups,
+                                page contents, conferences, bootcamps, and events.
+                            </p>
                         </div>
                         <div className="input-group mt-3 mb-3 pe-5">
-                            <input type="text" className="form-control newsletter py-2" placeholder="Enter your email for newsletter" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                            <button className="btn btn-secondary text-white px-5" type="button" id="button-addon2">Register</button>
+                            <input
+                                type="text"
+                                className="form-control newsletter py-2"
+                                placeholder="Enter your email for newsletter"
+                                aria-label="Recipient's username"
+                                aria-describedby="button-addon2"
+                            />
+                            <button className="btn btn-secondary text-white px-5" type="button" id="button-addon2">
+                                Register
+                            </button>
                         </div>
                     </Col>
                     <Col lg={6} md={1} className="d-flex welcome-col-2">
-                        <div className='caret'></div>
-                        <div className='d-flex align-items-center justify-content-center'>
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/uHVWItKFs3M?controls=0&amp;autoplay=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <div className="caret"></div>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <iframe
+                                width="560"
+                                height="315"
+                                src="https://www.youtube.com/embed/uHVWItKFs3M?controls=0&amp;autoplay=1"
+                                title="YouTube video player"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
                         </div>
                     </Col>
                 </Row>
             </div>
-            <div className='d-xs-block d-lg-none welcome-col-1-bg'>
-                <div className='d-flex'>
-                    <h1 className='display-1'><span className="text-secondary">Fin</span>ance is our root<br /><span className="text-secondary">Tech</span>nology is our weapon</h1>
+            <div className="d-xs-block d-lg-none welcome-col-1-bg">
+                <div className="d-flex">
+                    <h1 className="display-1">
+                        <span className="text-secondary">Fin</span>ance is our root
+                        <br />
+                        <span className="text-secondary">Tech</span>nology is our weapon
+                    </h1>
                 </div>
-                <div className='d-flex'>
-                    <p className='text-secondary fs-2 pe-7'>RMIT VIETNAM FINTECH CLUB</p>
+                <div className="d-flex">
+                    <p className="text-secondary fs-2 pe-7">RMIT VIETNAM FINTECH CLUB</p>
                 </div>
-                <div className='d-flex justify-content-center pe-2'>
-                    <p className=''>RMIT Vietnam FinTech Club was launched with the goal to inspire, educate and increase the exposure of people to FinTech and digital disruption via our workshops, meetups, page contents, conferences, bootcamps, and events.</p>
+                <div className="d-flex justify-content-center pe-2">
+                    <p className="">
+                        RMIT Vietnam FinTech Club was launched with the goal to inspire, educate and increase the
+                        exposure of people to FinTech and digital disruption via our workshops, meetups, page contents,
+                        conferences, bootcamps, and events.
+                    </p>
                 </div>
                 <div className="input-group mt-3 mb-3 pe-5">
-                    <input type="text" className="form-control newsletter py-2" placeholder="Enter email for newsletter" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                    <button className="btn btn-secondary text-white" type="button" id="button-addon2">Register</button>
+                    <input
+                        type="text"
+                        className="form-control newsletter py-2"
+                        placeholder="Enter email for newsletter"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                    />
+                    <button className="btn btn-secondary text-white" type="button" id="button-addon2">
+                        Register
+                    </button>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Welcome
+export default Welcome;

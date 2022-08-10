c219c5dca75f921141078b111674897228f4b708
nguyenquang2362 <quang.nguyen@jrgvn.com>
2022-08-10 15:08:35 +0700

    fix header

--- components/Header.js
+++ components/Header.js
@@ -16,52 +16,52 @@ const Header = () => {
     };
     return (
         <>
-            <div className="d-none d-md-flex bg-primary justify-center align-items-center sticky-top">
-                <div className="px-0 mx-0" style={{ flex: "0 0 auto", width: "90%" }}>
+            <div className="d-none d-md-flex bg-primary justify-content-center align-items-center sticky-top">
+                <div className="px-0 mx-0 d-flex justify-content-center align-items-center w-100 container position-relative">
                     <Navbar
                         collapseOnSelect
                         expand="md"
                         variant="dark"
                         sticky="top"
                         bg="primary"
-                        className="justify-content-center pt-0"
+                        className="justify-content-center pt-0 w-100"
                     >
-                        <Nav className="nav py-0 col-12 col-md-auto mb-2 mb-md-0" style={{ alignItems: "center" }}>
-                            <Nav.Link href="#home" className="nav-link px-3">
-                                Home
-                            </Nav.Link>
-                            <Nav.Link href="#about" className="nav-link px-3 ">
-                                About us
-                            </Nav.Link>
-                            <Nav.Link href="#projects" className="nav-link px-3">
-                                Projects
-                            </Nav.Link>
-                            <Navbar.Brand
-                                href="/"
-                                className="px-2 text-dark text-decoration-none pt-0 pb-0 mx-3 d-flex justify-content-center ft-logo"
-                            >
-                                <img
-                                    src="/club-logo.png"
-                                    width={57}
-                                    height={48}
-                                    style={{ zIndex: 10 }}
-                                    className="mt-2"
-                                />
-                            </Navbar.Brand>
-                            <Nav.Link href="#events" className="nav-link px-3">
-                                Events
-                            </Nav.Link>
-                            <Nav.Link href="#blog" className="nav-link px-3">
-                                Blog
-                            </Nav.Link>
-                            <Nav.Link href="#more" className="nav-link ps-3">
-                                More
-                            </Nav.Link>
+                        <Nav className="nav py-0 col-12 col-md-auto mb-2 mb-md-0 w-100 justify-content-center">
+                            <div className="d-flex align-items-center w-25 flex-row-reverse">
+                                <Nav.Link href="#home" className="nav-link px-3">
+                                    Home
+                                </Nav.Link>
+                                <Nav.Link href="#about" className="nav-link px-3 ">
+                                    About us
+                                </Nav.Link>
+                                <Nav.Link href="#projects" className="nav-link px-3">
+                                    Projects
+                                </Nav.Link>
+                            </div>
+                            <div className="align-items-center ">
+                                <Navbar.Brand
+                                    href="/"
+                                    className="px-2 text-dark text-decoration-none pt-0 pb-0 mx-3 d-flex justify-content-center ft-logo"
+                                >
+                                    <img src="/club-logo.png" width={57} height={48} className="mt-2" />
+                                </Navbar.Brand>
+                            </div>
+                            <div className="d-flex align-items-center w-25">
+                                <Nav.Link href="#events" className="nav-link px-3">
+                                    Events
+                                </Nav.Link>
+                                <Nav.Link href="#blog" className="nav-link px-3">
+                                    Blog
+                                </Nav.Link>
+                                <Nav.Link href="#more" className="nav-link ps-3">
+                                    More
+                                </Nav.Link>
+                            </div>
                         </Nav>
                     </Navbar>
-                </div>
-                <div className="px-0 mx-0" style={{ flex: "0 0 auto", width: "7%" }}>
-                    <button className="mx-0 btn btn-secondary text-white sign-in-btn">Sign in</button>
+                    <div className="px-0 mx-0 position-absolute" style={{ zIndex: 1021, right: "0.75rem" }}>
+                        <button className="mx-0 btn btn-secondary text-white sign-in-btn">Sign in</button>
+                    </div>
                 </div>
             </div>
 

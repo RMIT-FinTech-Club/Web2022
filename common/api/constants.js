fe54a41dd967abcb3afabbb3beee21600050aa3a
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-09-30 16:36:58 +0700

    Fetch moments (#15)

---
+++ common/api/constants.js
@@ -0,0 +1,8 @@
+const local = false;
+export const API_URL = local ? "http://localhost:1337/api" : "https://rmit-fintech-club-web-cms.onrender.com/api";
+
+export const DEPARTMENTS_ROUTE = "/departments";
+export const MEMBERS_ROUTE = "/members";
+export const EXECUTIVES_ROUTE = "/executives";
+export const EVENTS_ROUTE = "/events";
+export const MOMENTS_ROUTE = "/moments";

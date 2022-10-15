4edb01270c98b33fab460b692718b95ded7112e5
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-10-16 00:23:14 +0700

    Fix nav bar

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,18 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Button from "react-bootstrap/Button";
-import { useRouter } from "next/router";
 
 const Header = () => {
+    const navs = [
+        { id: "home", name: "Home", url: "/" },
+        { id: "about-us", name: "About us", url: "/about-us" },
+        { id: "projects", name: "Projects", url: "/projects" },
+        { id: "events", name: "Events", url: "/events" },
+        { id: "memorable-moments", name: "Moments", url: "/memorable-moments" },
+        { id: "more", name: "More", url: "/more" },
+    ];
+    const mid = Math.ceil(navs.length / 2);
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -13,144 +22,95 @@ const Header = () => {
     const toggleOffCanvas = () => {
         setShow((show) => !show);
     };
-
-    const router = useRouter();
-
     return (
         <>
-            <div className="d-none d-md-flex bg-primary justify-center align-items-center">
-                <div className="px-0 mx-0" style={{ flex: "0 0 auto", width: "100%" }}>
+            <div className="d-none d-lg-flex bg-primary justify-content-center align-items-center sticky-top">
+                <div className="px-0 mx-0 d-flex justify-content-center align-items-center w-100 container position-relative">
                     <Navbar
                         collapseOnSelect
                         expand="md"
                         variant="dark"
                         sticky="top"
                         bg="primary"
-                        className="justify-content-center pt-0"
+                        className="justify-content-center p-0 w-100"
                     >
-                        <Nav className="nav py-0 col-12 col-md-auto mb-2 mb-md-0" style={{ alignItems: "center" }}>
-                            <Nav.Link href="/" className="nav-link px-3" active={router.pathname == "/"}>
-                                Home
-                            </Nav.Link>
-                            <Nav.Link href="/about-us" className="nav-link px-3" active={router.pathname == "/about"}>
-                                About us
-                            </Nav.Link>
-                            <Nav.Link
-                                href="/projects"
-                                className="nav-link px-3"
-                                active={router.pathname == "/projects"}
-                            >
-                                Projects
-                            </Nav.Link>
-                            <Navbar.Brand
-                                href="/"
-                                className="px-2 text-dark text-decoration-none pt-0 pb-0 mx-3 d-flex justify-content-center ft-logo"
-                            >
-                                <img
-                                    src="/club-logo.png"
-                                    width={57}
-                                    height={48}
-                                    style={{ zIndex: 10 }}
-                                    className="mt-2"
-                                />
-                            </Navbar.Brand>
-                            <Nav.Link href="/events" className="nav-link px-3" active={router.pathname == "/events"}>
-                                Events
-                            </Nav.Link>
-
-                            <Nav.Link
-                                href="/memorable-moments"
-                                className="nav-link ps-3"
-                                active={router.pathname == "/memorable-moments"}
-                            >
-                                Moments
-                            </Nav.Link>
-
-                            <Nav.Link href="/more" className="nav-link px-3" active={router.pathname == "/more"}>
-                                More
-                            </Nav.Link>
+                        <Nav className="nav py-0 col-12 col-md-auto mb-2 mb-md-0 w-100 justify-content-center">
+                            <div className="d-flex align-items-center w-25 flex-row-reverse">
+                                {navs
+                                    .slice(0, mid)
+                                    .reverse()
+                                    .map((nav) => (
+                                        <div key={nav.id}>
+                                            <Nav.Link href={nav.url} className="nav-link px-3">
+                                                {nav.name}
+                                            </Nav.Link>
+                                        </div>
+                                    ))}
+                            </div>
+                            <div className="align-items-center pb-2">
+                                <Navbar.Brand
+                                    href="/"
+                                    className="px-2 text-dark text-decoration-none pt-0 pb-0 mx-3 d-flex justify-content-center ft-logo"
+                                >
+                                    <img src="/club-logo.png" className="mt-2" />
+                                </Navbar.Brand>
+                            </div>
+                            <div className="d-flex align-items-center w-25">
+                                {navs.slice(-mid).map((nav) => (
+                                    <div key={nav.id}>
+                                        <Nav.Link href={nav.url} className="nav-link px-3">
+                                            {nav.name}
+                                        </Nav.Link>
+                                    </div>
+                                ))}
+                            </div>
                         </Nav>
                     </Navbar>
                 </div>
             </div>
 
-            <div className="d-xs-block d-md-none bg-primary sticky-top pb-2">
+            <div className="d-xs-block d-lg-none bg-primary sticky-top">
                 <div className="d-flex justify-content-between align-items-center">
-                    <div className="ft-logo px-2 ms-3 me-0">
-                        <img src="/club-logo.png" width={57} height={48} style={{ zIndex: 10 }} className="mt-2" />
+                    <div className="ft-logo px-2 d-flex justify-content-center ms-3 mb-2">
+                        <img src="/club-logo.png" width={57} height={48} style={{ zIndex: 10 }} className="mt-1" />
                     </div>
                     <div className="me-1">
-                        <Button className="btn btn-white" onClick={handleShow}>
+                        <Button className="btn btn-white d-flex align-items-center" onClick={handleShow}>
                             <i className="bi bi-list mobile-nav-toggle"></i>
                         </Button>
                     </div>
                 </div>
             </div>
 
-            <div className="d-xs-block d-md-none">
-                <Offcanvas show={show} onHide={handleClose} placement="end" className="bg-primary">
+            <div className="d-xs-block d-lg-none">
+                <Offcanvas
+                    show={show}
+                    onHide={handleClose}
+                    placement="end"
+                    className="bg-primary"
+                    style={{ width: "77%" }}
+                >
                     <Offcanvas.Header>
-                        <Offcanvas.Title></Offcanvas.Title>
-                        <i className="bi bi-x-circle fs-1 text-white" onClick={handleClose}></i>
+                        <Offcanvas.Title className="text-secondary">Menu</Offcanvas.Title>
+                        <i className="bi bi-x-circle-fill fs-1 text-white close-icon" onClick={handleClose}></i>
                     </Offcanvas.Header>
-                    <Offcanvas.Body>
+                    <Offcanvas.Body className="p-0">
                         <Navbar
                             collapseOnSelect
                             expand="md"
                             variant="dark"
                             sticky="top"
                             bg="primary"
-                            className="justify-content-center pt-0"
+                            className="w-100 p-0"
                         >
                             <Nav className="nav py-0 col-12 col-md-auto mb-2 mb-md-0" style={{ alignItems: "center" }}>
-                                <Nav.Link
-                                    href="/"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/"}
-                                >
-                                    Home
-                                </Nav.Link>
-                                <Nav.Link
-                                    href="/about"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/about"}
-                                >
-                                    About us
-                                </Nav.Link>
-                                <Nav.Link
-                                    href="/projects"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/projects"}
-                                >
-                                    Projects
-                                </Nav.Link>
-                                <Nav.Link
-                                    href="/events"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/events"}
-                                >
-                                    Events
-                                </Nav.Link>
-                                <Nav.Link
-                                    href="/memorable-moments"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/memorable-moments"}
-                                >
-                                    Moments
-                                </Nav.Link>
-                                <Nav.Link
-                                    href="/more"
-                                    className="nav-link fs-3"
-                                    onClick={toggleOffCanvas}
-                                    active={router.pathname == "/more"}
-                                >
-                                    More
-                                </Nav.Link>
+                                {navs.map((nav) => (
+                                    <div key={nav.id}>
+                                        <Nav.Link href={nav.url} className="nav-link" onClick={toggleOffCanvas}>
+                                            {nav.name}
+                                        </Nav.Link>
+                                    </div>
+                                ))}
                             </Nav>
                         </Navbar>
                     </Offcanvas.Body>

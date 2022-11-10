6d9417e2f08b12d3388bb3bf101d852bd5264e30
Khai Truong <khai.truong@codelink.io>
2022-11-10 14:51:15 +0700

    load api url from env

--- common/api/constants.js
+++ common/api/constants.js
@@ -1,5 +1,11 @@
+const getRemoteApiUrl = () => {
+    const url = process.env.REACT_APP_API_URL;
+    if (!url) throw new Error("REACT_APP_API_URL is not defined");
+    return url;
+};
+
 const local = false;
-export const API_URL = local ? "http://localhost:1337/api" : "https://rmit-fintech-club-web-cms.onrender.com/api";
+export const API_URL = local ? "http://localhost:1337/api" : getRemoteApiUrl();
 
 export const DEPARTMENTS_ROUTE = "/departments";
 export const MEMBERS_ROUTE = "/members";
